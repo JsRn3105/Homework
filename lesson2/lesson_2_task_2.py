31cf488f211d1c1a1006549098ebca6f05b491b7
JsRn3105 <31may97@mail.ru>
2024-10-18 00:29:16 +0300

    Папка со второй домашкой

---
+++ lesson2/lesson_2_task_2.py
@@ -0,0 +1,6 @@
+def is_year_leap(year):
+    return year % 4 == 0
+year = int(input("Введите год: "))
+is_leap = is_year_leap(year)
+print(f"Год {year}: {is_leap}")
+
---
+++ lesson2/lesson_2_task_3.py
@@ -0,0 +1,9 @@
+import math
+
+def square(side):
+    area = side * side
+    return math.ceil(area)
+side_length = int(input("Введите сторону квадрата: "))
+area_of_square = square(side_length)
+
+print(f"Площадь квадрата: {area_of_square}")
